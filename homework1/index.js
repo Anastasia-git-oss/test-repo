bdea84fcff579c261005d7c0eaa4d5cdc32fd420
Anastasia T <tarasenkoanastasia66@hotmail.com>
2024-11-29 21:57:22 +0300

    index.js

---
+++ homework1/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+let x = 25;
+console.log(x);
+typeof 25;
+
+let y = 100;
+console.log(y);
+typeof 100;
+
+let add = x + y;
+console.log(add);
+
+let subtr = y - x;
+console.log(subtr);
+
+let multipl = x * y;
+console.log(multipl);
+
+let division = y / x;
+console.log(division);
+
+let degree = y ** 3;
+console.log(degree);
+
+let root = Math.sqrt(y);
+console.log(root);
+
+let string = "4";
+typeof string;
+
+
+let number = Number(string);
+typeof number;
+
+console.log(string);
+
+console.log(number);
+
+console.log(string == division);
+console.log(string === division);
